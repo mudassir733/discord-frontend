e22971bcfbad67821c6a70f5b044b5367e835126
mudassir733 <deadlool45@gmail.com>
2025-04-09 22:46:37 +0500

    feat: login page ui and validation form complete

---
+++ src/app/[locale]/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// components
+import Login from '@/features/auth/components/login'
+
+function page() {
+    return (
+        <Login />
+    )
+}
+
+export default page
---
+++ src/features/auth/components/login.tsx
@@ -0,0 +1,110 @@
+"use client"
+import React from 'react'
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+import Link from 'next/link';
+import Image from 'next/image';
+
+// utils
+import { loginSchema, LoginFormData } from '@/lib/schemas'
+
+// assets
+import QrCodeImg from "@/assets/images/Qr.svg"
+
+// ui component
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const login = () => {
+    const form = useForm<LoginFormData>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = (data: LoginFormData) => {
+        console.log("Form submitted:", data);
+    };
+    return (
+        <div className='w-full h-fit md:h-screen bg-[#5865F2] bg-cover bg-center bg-no-repeat flex items-center justify-center flex-col p-4 md:p-0'>
+            <div className='flex flex-col md:w-[800px] w-full h-fit bg-[#36393F] p-10 shadow-lg rounded-md'>
+                <section className='flex flex-col md:flex-row items-center gap-14'>
+                    <div className='md:flex-2 w-full'>
+                        <div className='text-center pb-6'>
+                            <h2 className='text-[25px] !text-white'>Welcom back!</h2>
+                            <p className='text-[#A3A6AA]'>We're so excited to see you again!</p>
+                        </div>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Email or Phone Number</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} />
+                                            </FormControl>
+
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Password</FormLabel>
+                                            <FormControl>
+                                                <Input  {...field} />
+                                            </FormControl>
+
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <div className=' mt-1'>
+                                    <Link href="" className='text-[#00AFF4] text-sm hover:underline'>Forgot your password?</Link>
+                                </div>
+                                <div className='w-full mt-3'>
+                                    <Button type='submit' variant={"blueBtn"} size={"loginBtn"} className='w-full rounded-none py-3'>Login</Button>
+                                </div>
+
+                                <div className=' mt-3 flex gap-2'>
+                                    <span className='text-[#A3A6AA] text-sm'>Need An Account?</span>
+                                    <Link className='text-[#00AFF4] text-sm hover:underline' href="">Register</Link>
+
+                                </div>
+                            </form>
+                        </Form>
+                    </div>
+
+                    <div className='flex-1'>
+                        <div className='flex flex-col items-center justify-center'>
+                            <div className='p-2 bg-[#fff] rounded-sm'>
+                                <Image src={QrCodeImg} alt='QR code image' />
+                            </div>
+                            <h2 className='text-[25px] text-center pt-2 !text-white'>Log in with QR Code</h2>
+                            <p className='text-[#A3A6AA] text-center text-sm'>Scan this with the Scan this with the app to log in instantly.</p>
+                        </div>
+                    </div>
+
+                </section>
+            </div>
+        </div >
+    )
+}
+
+
+export default login
